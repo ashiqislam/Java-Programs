2c1d466f28e17c53a04446cf0479750afe02b8c6
Ashiq Islam <33470390+ashiqislam@users.noreply.github.com>
2017-11-22 19:22:05 -0500

    Update (EqualsIgnoreCase, StartsWith, Restart Program).java

--- (EqualsIgnoreCase, StartsWith, Restart Program).java
+++ (EqualsIgnoreCase, StartsWith, Restart Program).java
@@ -6,15 +6,15 @@ public class Loops{
     public static void main(String[] args) {
       String restart;
         do {
-            Scanner inputz = new Scanner(System.in);
+            Scanner input = new Scanner(System.in);
             char c;
             System.out.println("Please enter a letter: ");
-            c = inputz.next().toUpperCase().charAt(0);
+            c = input.next().toUpperCase().charAt(0);
             System.out.println("You entered " + c);
             
             System.out.println("Do you want to restart the program? Enter the letter Y to restart:");
             System.out.println("Enter the Letter E to Exit"); 
-            restart = inputz.next();
+            restart = input.next();
             
             if (restart.toUpperCase().startsWith("E"))
             {
@@ -24,7 +24,7 @@ public class Loops{
             while (!restart.toUpperCase().startsWith("Y"))
             {
                 System.out.println("Invalid entry, Please try again: ");
-                restart = inputz.next();
+                restart = input.next();
             }
 
         }while (restart.toUpperCase().startsWith("Y"));
