a7b5f68ad52e8f4d2436f3ac057e85bddf3174c6
Ashiq Islam <33470390+ashiqislam@users.noreply.github.com>
2018-02-14 15:22:13 -0500

    Add files via upload

---
+++ ProgrammingPractice/Palindrome.java
@@ -0,0 +1,21 @@
+public class Palindrome {
+    public static boolean isPalindrome(String word) {
+    	if (word.length() <= 1) return true;
+    	word = word.toLowerCase();
+    	
+    	for (int i = 0; i < word.length()-1; i++)
+    	{
+    		if (word.charAt(0) == word.charAt(word.length()-1))
+    		{
+    			word = word.substring(1, word.length()-1);
+    		}
+    		else return false;
+    	}
+    	
+    	return true;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(Palindrome.isPalindrome("Deleveled"));
+    }
+}
---
+++ ProgrammingPractice/Path.java
@@ -0,0 +1,66 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.List;
+
+public class Path 
+{
+    private String path;
+
+    public Path(String path) 
+    {
+        this.path = path;
+    }
+
+    public String getPath() 
+    {
+        return path;
+    }
+
+    public void cd(String newPath) 
+    {
+        if (newPath.equals("/") || newPath.isEmpty()) path = "/";
+        else
+        {   // put each part of the newPath, excluding slashes, into an arrayDeque.
+        	String[] manip = newPath.split("/");
+        	List<String> list = Arrays.asList(manip);
+        	ArrayDeque<String> newDeque = new ArrayDeque<String>(list);
+        	if(newDeque.getFirst().isEmpty()) newDeque.removeFirst(); // remove blank at the front, if exists.
+        	System.out.println(newDeque); // In example, prints [.., x]
+        	
+        	// Same with the original path.
+        	manip = path.split("/");
+        	list = Arrays.asList(manip);
+        	ArrayDeque<String> pathDeque = new ArrayDeque<String>(list);
+        	if(pathDeque.getFirst().isEmpty()) pathDeque.removeFirst(); // remove blank at front, if exists.
+        	System.out.println(pathDeque); // In example, prints [a, b, c, d]
+        	
+        	// we can now manipulate both the path and newPath strings.
+        	while(!newDeque.isEmpty())
+        	{
+        		if (newDeque.getFirst().equals(".."))
+        		{
+        			pathDeque.removeLast();
+        		}
+        		else
+        		{
+        			pathDeque.addLast(newDeque.getFirst());
+        		}
+        		newDeque.removeFirst();
+        	}
+        	
+        	
+        	path = "";
+        	while (!pathDeque.isEmpty())
+        	{
+        		path += "/" + pathDeque.removeFirst();
+        	}
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        Path path = new Path("/a/b/c/d");
+        path.cd("../x");
+        System.out.println(path.getPath()); // should return '/a/b/c/x'
+    }
+}
