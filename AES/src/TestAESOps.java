8a1addce7ca921753c138d8ed5afbad80f7135fa
Ashiq Islam <ashiq08401@gmail.com>
2018-11-13 00:05:41 -0500

    Update TestAESOps.java

--- AES/src/TestAESOps.java
+++ AES/src/TestAESOps.java
@@ -6,20 +6,6 @@
 public class TestAESOps {
 	
 	public static void main(String[] args) {
-		// Write some code to test your various methods.
-		// You might consider using homework problems as a source of test cases since you can compare
-		// output to what you worked through by hand.
-		
-		// In Java, if you want to output an integer value in hexadecimal, you can use printf as follows:
-		// int value = SOME INTEGER VALUE;
-		// System.out.printf("%02x", value);
-		// The % starts specifying formatting.  The 02 means use 2 places and pad 1 digit numbers with a leading 0.
-		// The x means use hexadecimal.  printf doesn't put a new line character.  If you want to start a new line after,
-		// System.out.printf("%02x\n", value);  
-		// If you want to output multiple values with a single printf (assume they're called value1, value2, etc)
-		// then in the String that is the first parameter, you'd have formatting requirements for each as follows:
-		// System.out.printf("%02x %02x %02x %02x\n", value1, value2, value3, value4);
-		
 		
 		int[][] matrix = new int[][] { 
 			{ 0x08, 0x10, 0x35, 0xa6 }, 
